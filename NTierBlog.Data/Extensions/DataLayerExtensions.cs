5e6e9ee88b0d9d1259350986748fc01ffc29750a
batuhanorkunn <batuhanorkunn@gmail.com>
2025-05-10 13:31:29 +0300

    Generic repository pattern ve dependency injection tamamlandı.

---
+++ NTierBlog.Data/Extensions/DataLayerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NTierBlog.Data.Repositories.Abstracts;
+using NTierBlog.Data.Repositories.Concretes;
+
+namespace NTierBlog.Data.Extensions
+{
+	public static class DataLayerExtensions
+	{
+		public static IServiceCollection LoadDataLayerExtension(this IServiceCollection services, IConfiguration config)
+		{
+			services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+				return services;
+		}
+	}
+}
--- NTierBlog.Web/Program.cs
+++ NTierBlog.Web/Program.cs
@@ -1,8 +1,10 @@
 using Microsoft.EntityFrameworkCore;
-using NTierBlog.Data.Context;  
+using NTierBlog.Data.Context;
+using NTierBlog.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.LoadDataLayerExtension(builder.Configuration);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); 
